da341a1b83ebce103b30b54ed5b442ac6ada6f3d
asdenis <asdenis@msn.com>
2026-02-09 22:36:54 -0300

    delete vercel.json

--- next.config.js
+++ next.config.js
@@ -27,6 +27,10 @@ const nextConfig = {
             key: 'Access-Control-Allow-Credentials',
             value: 'true',
           },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
         ],
       },
     ];
